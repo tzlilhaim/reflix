b1d3993d1507c1ffe939969307403ab7df93c996
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-06 09:53:02 +0300

    Added link to catalog from landing

--- reflix/src/components/landing/User.js
+++ reflix/src/components/landing/User.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 
 class User extends Component {
   render() {
     return (
-      <div className="user" data-id={this.props.id} data-color={this.props.color}>
-        <span className="username">{this.props.name}</span>
-      </div>
+      <Link to={`/catalog`}>
+        <div
+          className="user"
+          data-id={this.props.id}
+          data-color={this.props.color}
+        >
+          <span className="username">{this.props.name}</span>
+        </div>
+      </Link>
     )
   }
 }
